2a388c7a1050a3d0e3ba3b58baf64c4d791e6d9d
Jimmy Van <jvan709@gmail.com>
2025-02-19 19:20:32 -0700

    fix contact header and skill tags

--- src/components/SkillTag.svelte
+++ src/components/SkillTag.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button
-	class={'flex h-full cursor-default flex-row items-center justify-center rounded-md border-2 border-black  font-nunito font-bold transition-all dark:border-custom-white-theme dark:bg-gray-900/90 dark:hover:text-black' +
+	class={' h-full cursor-default rounded-md border-2 border-black  font-nunito font-bold transition-all dark:border-custom-white-theme dark:bg-gray-900/90 dark:hover:text-black' +
 		(index % 2 === 0
 			? 'hover:bg-green-200 dark:hover:bg-green-300'
 			: 'hover:bg-blue-200 dark:hover:bg-blue-300')}
@@ -13,9 +13,11 @@
 	data-aos-duration="500"
 	data-aos-delay="500"
 >
-	{#if skill.icon}
-		<img class={'m-2 h-5'} src={'/assets/icons/' + skill.icon} alt={skill.label + ' Icon'} />
-	{/if}
+	<div class="flex flex-row items-center justify-center p-1">
+		{#if skill.icon}
+			<img class={'m-2 h-5'} src={'/assets/icons/' + skill.icon} alt={skill.label + ' Icon'} />
+		{/if}
 
-	<span class="px-3 py-2 tracking-wide">{skill.label}</span>
+		<span class="px-2 py-2 tracking-wide">{skill.label}</span>
+	</div>
 </button>
